58869005cbd1e6406f2c94436800af08c9abebed
Robert Marriott <robmarriott26@gmail.com>
2019-04-01 00:46:18 +0100

    Responsive layout improvements.

--- src/components/AlbumDetails.vue
+++ src/components/AlbumDetails.vue
@@ -1,62 +1,72 @@
 <template>
-  <v-container>
-    <v-layout>
-      <v-flex xs12>
+  <div class="albumDetails">
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
 
-        <v-card v-if="album">
-          <v-container>
-            <v-layout row wrap>
-              <v-flex xs6 sm4 md3 pr-4>
-               <v-img :src="album.images[0].url" style="width: 100%;" aspect-ratio="1" class="grey lighten-2"></v-img>
-              </v-flex>
-
-              <v-flex sx6 sm8 md9>
-                <h1 class="display-2 font-weight-black ">{{album.name}}</h1>
-                <router-link :to="{ name: 'artistdetails', params: { id: album.artists[0].id } }" class="link-green">
-                  <h2>{{album.artists[0].name}}</h2>
-                </router-link>
-                <p>{{album.release_date | formatDateToYear}}</p>
-                <p>{{album.total_tracks}} songs</p>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card>
-
-        <v-slide-y-transition>
-          <v-card v-if="tracks">
-            <v-card-title class="headline font-weight-regular">Tracklist</v-card-title>
+          <v-card v-if="album">
             <v-container>
               <v-layout row wrap>
-                <v-flex xs12>
-                  <v-list two-line>
-                    <a v-for="track in tracks.items" v-bind:href="track.href" :key="track.id" class="link-green" title="Play Track">
-                      <v-list-tile avatar class="list-item">
-                        <v-list-tile-action>
-                          <v-icon color="green">play_circle_filled</v-icon>
-                        </v-list-tile-action>
+                <v-flex xs6 sm4 md3 pr-4 pb-4>
+                 <v-img :src="album.images[0].url" style="width: 100%;" aspect-ratio="1" class="grey lighten-2"></v-img>
+                </v-flex>
 
-                        <v-list-tile-content>
-                          <v-list-tile-title>
-                            {{track.track_number}}. {{track.name}}
-                          </v-list-tile-title>
-                          <v-list-tile-sub-title>
-                            {{track.duration_ms | formatTime}}
-                          </v-list-tile-sub-title>
-                        </v-list-tile-content>
-                      </v-list-tile>
-
-                      <v-divider></v-divider>
-                    </a>
-                  </v-list>
+                <v-flex xs12 sm8 md9>
+                  <h1 class="display-2 font-weight-black ">{{album.name}}</h1>
+                  <router-link :to="{ name: 'artistdetails', params: { id: album.artists[0].id } }" class="link-green">
+                    <h2>{{album.artists[0].name}}</h2>
+                  </router-link>
+                  <p>{{album.release_date | formatDateToYear}}</p>
+                  <p>{{album.total_tracks}} songs</p>
                 </v-flex>
               </v-layout>
             </v-container>
           </v-card>
-        </v-slide-y-transition>
 
-      </v-flex>
-    </v-layout>
-  </v-container>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+
+          <v-slide-y-transition>
+            <v-card v-if="tracks">
+              <v-card-title class="headline font-weight-regular">Tracklist</v-card-title>
+              <v-container>
+                <v-layout row wrap>
+                  <v-flex xs12>
+                    <v-list two-line>
+                      <a v-for="track in tracks.items" v-bind:href="track.href" :key="track.id" class="link-green" title="Play Track">
+                        <v-list-tile avatar class="list-item">
+                          <v-list-tile-action>
+                            <v-icon color="green">play_circle_filled</v-icon>
+                          </v-list-tile-action>
+
+                          <v-list-tile-content>
+                            <v-list-tile-title>
+                              {{track.track_number}}. {{track.name}}
+                            </v-list-tile-title>
+                            <v-list-tile-sub-title>
+                              {{track.duration_ms | formatTime}}
+                            </v-list-tile-sub-title>
+                          </v-list-tile-content>
+                        </v-list-tile>
+
+                        <v-divider></v-divider>
+                      </a>
+                    </v-list>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
+          </v-slide-y-transition>
+
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
--- src/components/ArtistDetails.vue
+++ src/components/ArtistDetails.vue
@@ -1,58 +1,67 @@
 <template>
-  <v-container>
-    <v-layout>
-      <v-flex xs12>
+  <div class="artistDetails">
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
 
-        <v-card v-if="artist">
-          <v-container>
-            <v-layout row wrap>
-              <v-flex xs6 sm4 md3 pr-4>
-               <v-img :src="artist.images[0].url" style="width: 100%;" aspect-ratio="1" class="grey lighten-2"></v-img>
-              </v-flex>
+          <v-card v-if="artist">
+            <v-container>
+              <v-layout row wrap>
+                <v-flex xs6 sm4 md3 pr-4 pb-4>
+                 <v-img :src="artist.images[0].url" style="width: 100%;" aspect-ratio="1" class="grey lighten-2"></v-img>
+                </v-flex>
 
-              <v-flex sx6 sm8 md9>
-                <h1 class="display-2 font-weight-black ">{{artist.name}}</h1>
-                <div v-if="albums !== undefined">
-                  <h2>{{albums.items.length}} Albums</h2>
-                </div>
-                <p>{{artist.followers.total}} followers</p>
-                <p><a v-bind:href="artist.external_urls.spotify" class="link-green" target="_blank">View on Spotify</a></p>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card>
+                <v-flex xs12 sm8 md9>
+                  <h1 class="display-2 font-weight-black ">{{artist.name}}</h1>
+                  <div v-if="albums !== undefined">
+                    <h2>{{albums.items.length}} Albums</h2>
+                  </div>
+                  <p>{{artist.followers.total}} followers</p>
+                  <p><a v-bind:href="artist.external_urls.spotify" class="link-green" target="_blank">View on Spotify</a></p>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
 
-        <v-card>
-          <v-card-title class="headline font-weight-regular">Albums</v-card-title>
-          <v-container grid-list-sm fluid>
-            <v-layout row wrap v-if="albums">
-              <v-flex v-for="album in orderBy(albums.items, 'release_date', -1)" :key="album.id" xs6 sm4 md2 d-flex pa-2 pb-4>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
 
-                <v-card flat tile class="d-flex">
-                  <router-link :to="{ name: 'albumdetails', params: { id: album.id } }" class="link-green">
-                    <v-img :src="album.images[0].url" aspect-ratio="1" class="black lighten-2">
-                      <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
-                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                      </v-layout>
-                    </v-img>
-                    <div class="album-image-title">
-                      <p>{{album.release_date | formatDateToYear}}</p>
-                      <h3>{{album.name}}</h3>
-                    </div>
-                  </router-link>
-                </v-card>
-                
-              </v-flex>
-            </v-layout>
-            <v-layout row wrap v-else>
-              <p>No albums to show.</p>
-            </v-layout>
-          </v-container>
-        </v-card>
+          <v-card>
+            <v-card-title class="headline font-weight-regular">Albums</v-card-title>
+            <v-container grid-list-sm fluid>
+              <v-layout row wrap v-if="albums">
+                <v-flex v-for="album in orderBy(albums.items, 'release_date', -1)" :key="album.id" xs6 sm4 md3 lg2 d-flex pa-2 pb-4>
 
-      </v-flex>
-    </v-layout>
-  </v-container>
+                  <v-card flat tile class="d-flex">
+                    <router-link :to="{ name: 'albumdetails', params: { id: album.id } }" class="link-green">
+                      <v-img :src="album.images[0].url" aspect-ratio="1" class="black lighten-2">
+                        <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
+                          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                        </v-layout>
+                      </v-img>
+                      <div class="album-image-title">
+                        <p>{{album.release_date | formatDateToYear}}</p>
+                        <h3>{{album.name}}</h3>
+                      </div>
+                    </router-link>
+                  </v-card>
+                  
+                </v-flex>
+              </v-layout>
+              <v-layout row wrap v-else>
+                <p>No albums to show.</p>
+              </v-layout>
+            </v-container>
+          </v-card>
+
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
